0adb5d0d102a6fa2aac8616bd48f9b9d945dc9ed
Abhay Pandey <116704975+Aloneking789@users.noreply.github.com>
2024-10-29 13:46:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,18 +2,16 @@
 const express = require('express');
 const cors = require('cors'); // Import cors
 const mongoose = require('mongoose');
-const dotenv = require('dotenv').config()
+const dotenv = require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
-
-app.use(cors());
 
 // Middleware for parsing JSON requests
+app.use(cors());
 app.use(express.json());
 
 // Connect to your MongoDB database
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.error('MongoDB connection error:', err));
 
@@ -29,7 +27,7 @@ const Contact = mongoose.model('Contact', contactSchema);
 
 // POST endpoint to save contact data
 app.post('/api/contact', async (req, res) => {
-    console.log("Request")
+    console.log("Request received");
     const { name, email, phone, message } = req.body;
 
     try {
@@ -42,7 +40,5 @@ app.post('/api/contact', async (req, res) => {
     }
 });
 
-// Start the server
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+// Export the app for Vercel
+module.exports = app;
